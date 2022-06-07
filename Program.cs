0905ff281f53475f165fdb4746d952e7fd946605
Bo Bendtsen <bben@dr.dk>
2022-06-07 17:04:02 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using BookBeat.Akamai.EdgeAuthToken;
+
+if (args.Length!=2) {
+    Console.Error.WriteLine("Usage: dotnet run <key> <url-to-unlock>");
+    Environment.Exit(-1);
+}
+
+var key = args[0];
+var uri = new Uri(args[1]);
+
+var uriBuilder = new UriBuilder(uri);
+
+var tokenConfig = new AkamaiTokenConfig
+{
+	Window = 86400, // 24h, Time to live (in seconds)
+	Acl = uriBuilder.Path, // Access control list containing token permissions
+	Key = key // Encryption key
+};
+
+var tokenGenerator = new AkamaiTokenGenerator();
+
+var token = tokenGenerator.GenerateToken(tokenConfig);
+uriBuilder.Query = "hdnea="+token;
+
+Console.WriteLine("Unlocked uri:");
+Console.WriteLine(uriBuilder.Uri.ToString());
+var hc = new HttpClient();
+var req = new HttpRequestMessage(HttpMethod.Head, uriBuilder.Uri);
+var res = await hc.SendAsync(req);
+Console.WriteLine($"Checked : {res.StatusCode}");
---
+++ README.md
@@ -0,0 +1,5 @@
+# DR AkamaiEdgeAuthUnlockDemo
+
+Req: .net6.0
+
+Usage: `dotnet run <key> <url-to-unlock>`
